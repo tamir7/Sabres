be732c557513e2731e1519405d5955d95b9dcf87
Tamir Shomer <tamir7@gmail.com>
2015-05-16 16:51:16 +0300

    Added javadoc to CountCallback

--- sabres/src/main/java/com/sabres/CountCallback.java
+++ sabres/src/main/java/com/sabres/CountCallback.java
@@ -16,6 +16,38 @@
 
 package com.sabres;
 
+/**
+ * A CountCallback is used to run code after a SabresQuery is used to count objects matching a
+ * query in a background thread.
+ * <p>
+ * The easiest way to use a CountCallback is through an anonymous inner class.
+ * Override the done function to specify what the callback should do after the count is complete.
+ * The done function will be run in the UI thread, while the count happens in a background thread.
+ * This ensures that the UI does not freeze while the fetch happens.
+ * <p>
+ * For example, this sample code counts objects of class "MyClass".
+ *
+ * <pre>
+ * {@code
+ * SabresQuery<MyClass> query = SabresQuery.getQuery("MyClass.class);
+ * query.countInBackground(new CountCallback() {
+ *     public void done(int count, SabresException e) {
+ *         if (e == null) {
+ *             objectsWereCountedSuccessfully(count);
+ *         } else {
+ *             objectCountingFailed();
+ *         }
+ *     }
+ * });
+ * }
+ * </pre>
+ */
 public interface CountCallback {
+    /**
+     * Override this function with the code you want to run after the count is complete.
+     *
+     * @param count The number of objects matching the query.
+     * @param e     The exception raised by the count, or null if it succeeded.
+     */
     void done(Long count, SabresException e);
 }
