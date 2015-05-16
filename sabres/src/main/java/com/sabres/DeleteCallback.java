9250a40cea471d23abd9a1b5279533b34f3f0c40
Tamir Shomer <tamir7@gmail.com>
2015-05-16 16:56:19 +0300

    Added javadoc to DeleteCallback

--- sabres/src/main/java/com/sabres/DeleteCallback.java
+++ sabres/src/main/java/com/sabres/DeleteCallback.java
@@ -16,6 +16,34 @@
 
 package com.sabres;
 
+/**
+ * A DeleteCallback is used to run code after saving a SabresObject in a background thread.
+ * <p>
+ * The easiest way to use a DeleteCallback is through an anonymous inner class.
+ * Override the done function to specify what the callback should do after the delete is complete.
+ * The done function will be run in the UI thread, while the delete happens in a background thread.
+ * This ensures that the UI does not freeze while the delete happens.
+ *
+ * For example, this sample code deletes the object myObject.
+ * <pre>
+ * {@code
+ * myObject.deleteInBackground(new DeleteCallback() {
+ *     public void done(SabresException e) {
+ *         if (e == null) {
+ *             myObjectWasDeletedSuccessfully();
+ *         } else {
+ *             myObjectDeleteDidNotSucceed();
+ *         }
+ *     }
+ * });
+ * }
+ * </pre>
+ */
 public interface DeleteCallback {
+    /**
+     * Override this function with the code you want to run after the delete is complete.
+     *
+     * @param e The exception raised by the delete, or null if it succeeded.
+     */
     void done(SabresException e);
 }
